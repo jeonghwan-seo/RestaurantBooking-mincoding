bb482c379ffcc88510d2146e1a05f38d71191757
hwany <jeonghwan.seo@mincoding.co.kr>
2024-05-02 11:20:44 +0900

    Step6-1. SMS Test Double 만들기

--- python/communication.py
+++ python/communication.py
@@ -7,3 +7,12 @@ class MailSender:
 class SmsSender:
     def send(self, schedule):
         print(f"Sending SMS to {schedule.get_customer().phone_number} for schedule at {schedule.get_date_time()}")
+
+
+class TestableSmsSender(SmsSender):
+    def send(self, schedule):
+        print("테스트용 SmsSender에서 send 메서드 실행됨")
+        self.__send_method_is_called = True
+
+    def is_send_method_is_called(self):
+        return self.__send_method_is_called
