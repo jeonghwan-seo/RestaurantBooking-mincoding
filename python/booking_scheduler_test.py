6c9d6017b3ae79633b7b1c806fd8104b0f2b7a74
hwany <jeonghwan.seo@mincoding.co.kr>
2024-05-02 11:24:10 +0900

    Step6-2. Test Double 로 UnitTest하기

--- python/booking_scheduler_test.py
+++ python/booking_scheduler_test.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import datetime, timedelta
 
+from python.communication import TestableSmsSender
 from schedule import Customer, Schedule
 from booking_scheduler import BookingScheduler
 
@@ -54,7 +55,16 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_예약완료시_SMS는_무조건_발송(self):
-        pass
+        #arrange
+        testable_sms_sender = TestableSmsSender()
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.set_sms_sender(testable_sms_sender)
+
+        # act
+        self.booking_scheduler.add_schedule(schedule)
+
+        # assert
+        self.assertTrue(testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
